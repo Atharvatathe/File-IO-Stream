b73b127e9a5bed28eb70f3afb0fbe072fae087c1
Atharvatathe <atharva.tathe55@gmail.com>
2021-04-30 21:01:04 +0530

    Added: Create Watch Service to watch perticular directory

--- main/java/EmployeePayrollService.java
+++ main/java/EmployeePayrollService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
+
+    public enum IOService{CONSOLE_IO,FILE_IO,DB_IO,REST_IO}
     private List<EmployeePayrollData> employeePayrollList;
 
     public EmployeePayrollService(List<EmployeePayrollData>employeePayrollList){
@@ -16,7 +18,7 @@ public class EmployeePayrollService {
         EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
         Scanner consoleInputReader = new Scanner(System.in);
         employeePayrollService.readEmployeePayRollData(consoleInputReader);
-        employeePayrollService.writeEmployeePayRollData();
+        employeePayrollService.writeEmployeePayRollData(IOService.CONSOLE_IO);
     }
 
     private void readEmployeePayRollData(Scanner consoleInputReadr){
@@ -29,8 +31,23 @@ public class EmployeePayrollService {
 
     }
 
-    private void writeEmployeePayRollData(){
-    System.out.println(employeePayrollList);
+    public void writeEmployeePayRollData(EmployeePayrollService.IOService ioService){
+        if(ioService.equals(IOService.CONSOLE_IO))
+            System.out.println("\nWriting Employee Payroll Roaster to console\n"+ employeePayrollList);
+        else if(ioService.equals(IOService.FILE_IO))
+            new EmployeePayrollFileIOService().writeData(employeePayrollList);
+
+    }
+    public void printData(IOService ioService) {
+        if(ioService.equals(IOService.FILE_IO))
+            new EmployeePayrollFileIOService().printData();
+
+    }
+
+    public long countEntries(IOService ioService) {
+        if(ioService.equals(IOService.FILE_IO))
+            return new EmployeePayrollFileIOService().countEntries();
+        return 0;
     }
 }
 
